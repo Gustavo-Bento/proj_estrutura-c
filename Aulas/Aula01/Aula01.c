c9e1d4f35b26d44576f92adfd622d58440be0bf8
Gustavo Ferreira <gfmb94@gmail.com>
2023-10-17 20:50:15 -0300

    Aula 01 - Array unidimensional

---
+++ Aulas/Aula01/Aula01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Vetor unidimensional
+
+int main(){
+
+    int idade;
+    char sexo;
+    float n1;
+
+    int num1[10];
+    int num2[] = {1,5,9,15,43};
+    int num3[5] = {4,7,5};
+    int num4[5] = {1,2,3,4,5};
+    int num5[5] = {0};
+
+    char letras[100];
+    char vogais[5] = {'a','e','i','o','u'};
+
+    float notas[5] = {7.5,5.3,4.8,8.5,9};
+
+    printf("%d ",num2[0]);
+    printf("%d ",num2[1]);
+
+    return 0;
+}
