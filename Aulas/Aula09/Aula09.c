4bff929805d7ebd005d2120a5f46182a84161631
Gustavo Ferreira <gfmb94@gmail.com>
2023-10-18 00:03:00 -0300

    Aula 09 - Pilha, metodo push

---
+++ Aulas/Aula09/Aula09.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Pilha - Metodo push
+
+typedef struct{
+    int dia, mes, ano;
+}Data;
+
+typedef struct{
+    char nome[50];
+    Data data;
+}Pessoa;
+
+typedef struct no{
+    Pessoa p;
+    struct no *proximo;
+}No;
+
+Pessoa ler_pessoa(){
+    Pessoa p;
+
+    printf("\nDigite nome e data de nascimento dd mm aaaa:\n");
+    scanf("%49[^\n]%d%d%d", p.nome, &p.data.dia, &p.data.mes, &p.data.ano);
+    return p;
+}
+
+void imprimir_pessoa(Pessoa p){
+    printf("\nNome: %a\nData: %2d/%d/%4d\n", p.nome, p.data.dia, p.data.mes, p.data.ano);
+}
+
+//Função push(empilhar)
+
+No* push(No *topo){
+    No *novo = malloc(sizeof(No));
+
+    if(novo){
+        novo->p = ler_pessoa();
+        novo->proximo = topo;
+        return novo;
+    }else
+        printf("\nErro ao alocar memória...\n");
+        return NULL;
+}
+
+int main(){
+    No *topo = NULL;
+    int opcao;
+
+    do{
+        printf("\n0 - Sair\n1 - Empilhar\n2 - Desempilhar\n3 - Imprimir\n");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch (opcao){
+            case 1:
+                topo = empilhar(topo);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                if(opcao !=0)
+                    prinf("\nOpcao invalida!!!\n");
+        }
+    }while(opcao !=0);
+}
