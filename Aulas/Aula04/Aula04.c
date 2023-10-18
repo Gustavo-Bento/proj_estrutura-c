a614b99ea1d997ec3b2ef03cab7f0b4c678e5af1
Gustavo Ferreira <gfmb94@gmail.com>
2023-10-17 21:23:39 -0300

    Aula 04 - Introdução a matrizes

---
+++ Aulas/Aula04/Aula04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Matriz
+
+int main(){
+
+    int vet[10];
+
+    int mat1[3][3] ={1,2,4,5,6,7,8,9};
+
+    int mat2[][3] = {1,2,3,4,5,6,7};
+
+    int mat3[3][3] = {0};
+
+    float mat4[4][5];
+
+    char mat5[10][10];
+
+    return 0;
+}
