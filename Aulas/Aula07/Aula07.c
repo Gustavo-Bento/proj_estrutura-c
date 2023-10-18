f48b9d0bc49ee19b747c968cb32fa2a2bc3d8054
Gustavo Ferreira <gfmb94@gmail.com>
2023-10-17 22:27:46 -0300

    Aula 07 - Alocando uma matriz dinâmica

---
+++ Aulas/Aula07/Aula07.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Alocando matriz dinâmica
+
+int main()
+{
+
+    int **mat, i, j;
+
+    mat = malloc(4 * sizeof(int *));
+
+    for (i = 0; i < 4; i++)
+        mat[i] = malloc(3 * sizeof(int));
+
+    srand(time(NULL));
+
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 3; i++)
+            mat[i][j] = rand() % 100;
+
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 3; i++)
+            printf("%d ", mat[i][j]);
+    prinf("\n");
+
+    return 0;
+}
